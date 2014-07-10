d0a57ad14d90c6b15587dceac54db5e2d5fb758a
Kelly An <4ever.kellyan@gmail.com>
2014-07-09 22:31:48 -0700

    update setting.py username

--- OrderTrack/settings.py
+++ OrderTrack/settings.py
@@ -64,7 +64,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'order',
-		'USER': 'xxd',
+		'USER': 'kailian',
 		'PASSWORD': '',
 		'HOST': 'localhost',  # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
 		'PORT': '5432',
